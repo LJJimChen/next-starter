bbf6633a83c463cc4e4f9790d316177ad2a8f145
jim lj chen <lj.jim.chen@foxmail.com>
2022-07-23 17:22:06 +0800

    eslint-plugin-json&prettier&stylelint&tsc-files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,14 @@
 const path = require('path')
 
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => `"${path.relative(process.cwd(), f)}"`)
-    .join(' --file ')}`
+    `next lint --fix --file ${filenames
+        .map((f) => `"${path.relative(process.cwd(), f)}"`)
+        .join(' --file ')}`
 
+const tscCheckTypes = 'tsc-files --incremental false --noEmit'
+const prettierFix = 'prettier --write --ignore-unknown'
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+    '*.{js,jsx,ts,tsx}': [buildEslintCommand, tscCheckTypes, prettierFix],
+    '*.json': [prettierFix, 'eslint'],
+    '*.{css,scss}': ['stylelint --fix', prettierFix],
 }
