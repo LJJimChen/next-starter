45d39f7891c7d27e285fdb57e9b5b431a37cee41
jim lj chen <lj.jim.chen@foxmail.com>
2022-07-29 20:53:16 +0800

    custom webpack config for production sourcemap

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
-const { i18n } = require('./next-i18next.config')
+const { i18n } = require('./next-i18next.config');
+const { SourceMapDevToolPlugin } = require('webpack');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  i18n,
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    i18n,
+    webpack: (config, { isServer, dev }) => {
+        if (!isServer && !dev) {
+            const srcMapPlugin = new SourceMapDevToolPlugin({
+                filename: 'sourcemap/[file].map',
+            });
+            srcMapPlugin.sourceMappingURLComment = false;
+            config.plugins = [...config.plugins, srcMapPlugin];
+        }
+        return config;
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
